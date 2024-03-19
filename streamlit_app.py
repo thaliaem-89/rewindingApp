9c555b3c6a94a1ac187999e7f7b5f81f45617daf
thaliaem-89 <73491305+thaliaem-89@users.noreply.github.com>
2024-03-19 13:19:19 -0400

    add streamlit and supabase connection version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
-print('Hello world')
+# streamlit_app.py
+
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="all_variants", ttl="10m").execute()
+
+# Print results.
+for row in rows.data[1:10]:
+    st.write(f" machine {row['machine']} has a variant :{row['variant']}:")
